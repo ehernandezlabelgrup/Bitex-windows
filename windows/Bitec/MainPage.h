aebb4b66e1ee86161c30d773a51928fbf55991a1
Emilio Hernandez <eherna40@gmail.com>
2021-03-09 19:37:14 +0100

    dev

---
+++ windows/Bitec/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Bitec::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Bitec::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
